05fc71381e8ebd3f5250212ea7859890b10c17fa
G00392765@atu.ie <G00392765@atu.ie>
2023-10-06 14:48:49 +0100

    Implemented a method to handle GET requests to /registerUser/{name}/{email}

--- src/main/java/atu/cicd2/cicd2_week3_lab/UserController.java
+++ src/main/java/atu/cicd2/cicd2_week3_lab/UserController.java
@@ -1,13 +1,21 @@
 package atu.cicd2.cicd2_week3_lab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService myUserService;
+
+    public void userRegister(String name, String email){
+        myUserService.registerUser(name, email);
+    }
+
     @GetMapping("/registerUser/{name}/{email}")
     public void registerUsers(@PathVariable String name, @PathVariable String email){
-
+        userRegister(name, email);
     }
 }

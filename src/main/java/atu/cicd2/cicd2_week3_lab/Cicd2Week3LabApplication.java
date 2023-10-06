c653d1081d42022cfed1432abd48f4a568c8cb00
G00392765@atu.ie <G00392765@atu.ie>
2023-10-06 14:19:05 +0100

    Initial Commit

---
+++ src/main/java/atu/cicd2/cicd2_week3_lab/Cicd2Week3LabApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.cicd2_week3_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd2Week3LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd2Week3LabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/cicd2/cicd2_week3_lab/Cicd2Week3LabApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.cicd2_week3_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2Week3LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

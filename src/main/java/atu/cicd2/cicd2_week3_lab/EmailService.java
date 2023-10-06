1e9f8df602d1cd1b835f8567cd532d7223a2c27e
G00392765@atu.ie <G00392765@atu.ie>
2023-10-06 14:33:28 +0100

    Copied content from lecture slides

--- src/main/java/atu/cicd2/cicd2_week3_lab/EmailService.java
+++ src/main/java/atu/cicd2/cicd2_week3_lab/EmailService.java
@@ -1,4 +1,10 @@
 package atu.cicd2.cicd2_week3_lab;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+    public void sendEmail(String to, String message) {
+        //Code to send an email
+    }
 }

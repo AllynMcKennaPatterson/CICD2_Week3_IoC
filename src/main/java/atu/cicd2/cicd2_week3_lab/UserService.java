491979ee7e6e13ca4aed7633743f60fe489b5bc0
G00392765@atu.ie <G00392765@atu.ie>
2023-10-06 14:30:15 +0100

    Copied userClass content from lecture slides

--- src/main/java/atu/cicd2/cicd2_week3_lab/UserService.java
+++ src/main/java/atu/cicd2/cicd2_week3_lab/UserService.java
@@ -1,4 +1,18 @@
 package atu.cicd2.cicd2_week3_lab;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final EmailService emailService;
+
+    @Autowired
+    public UserService(EmailService emailService){
+        this.emailService = emailService;
+    }
+
+    public void registerUser(String username, String email) {
+        emailService.sendEmail(email, "Welcome to our platform " + username);
+    }
 }
